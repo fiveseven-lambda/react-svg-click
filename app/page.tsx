56263105a142f39d69aeab5a2423bf950fdeba4c
fiveseven <fiveseven.lambda@gmail.com>
2024-12-10 17:02:46 +0900

    anyだめ，ぜったい

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Home() {
   const [p, setP] = useState([100, 100]);
@@ -8,14 +8,22 @@ export default function Home() {
   const [isMouseOver, setIsMouseOver] = useState(false);
   const [isMouseDown, setIsMouseDown] = useState(false);
 
-  const setCoordinates = (event: any) => {
+  const setCoordinates = ({
+    clientX,
+    clientY,
+  }: {
+    clientX: number;
+    clientY: number;
+  }) => {
     if (svgRef.current !== null) {
       const boundingRect = svgRef.current.getBoundingClientRect();
-      const newX = event.clientX - boundingRect.x;
-      const newY = event.clientY - boundingRect.y;
+      const newX = clientX - boundingRect.x;
+      const newY = clientY - boundingRect.y;
       setP([newX, newY]);
     }
-  }
+  };
+  const handleMouse = (event: React.MouseEvent) => setCoordinates(event);
+  const handleTouch = (event: React.TouchEvent) => setCoordinates(event.changedTouches[0]);
 
   return <main className="container mx-auto">
     <p>({p[0]}, {p[1]})</p>
@@ -30,14 +38,14 @@ export default function Home() {
         onMouseLeave={() => setIsMouseOver(false)}
         onMouseDown={event => {
           setIsMouseDown(true);
-          setCoordinates(event);
+          handleMouse(event);
         }}
         onMouseUp={() => setIsMouseDown(false)}
         onMouseMove={event => {
-          if (isMouseDown) setCoordinates(event);
+          if (isMouseDown) handleMouse(event);
         }}
-        onTouchStart={setCoordinates}
-        onTouchMove={setCoordinates}
+        onTouchStart={handleTouch}
+        onTouchMove={handleTouch}
       />
       <circle cx={p[0]} cy={p[1]} r={10} fill="red" className="pointer-events-none"/>
     </svg>
